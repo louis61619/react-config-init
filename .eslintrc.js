da8a4e0ae57eb1d711c8e9ef489c9014c521c1ea
louis <louis61619@gmail.com>
2021-10-15 14:11:41 +0800

    style(prettier): change prettier in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+
+  extends: ['react-app', 'react-app/jest', 'prettier'],
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    'prettier/prettier': 'warn'
   }
 }
